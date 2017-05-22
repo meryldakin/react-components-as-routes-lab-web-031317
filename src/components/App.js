8cf397d6f5f1778417fae9036e66f5c1212279fe
meryldakin <meryl.k.dakin@gmail.com>
2017-05-22 13:41:02 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,12 @@ import React from 'react';
 import NavBar from './NavBar';
 
 const App = (props) => {
+
   return (
     <div>
     <div className="app">
-        {/* code goes here */}
-        {props.children}
+      <NavBar />
+      {props.children}
     </div>
     </div>
   );
